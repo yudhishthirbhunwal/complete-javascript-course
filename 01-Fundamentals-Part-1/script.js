4321da64e636cdfdf508483a7f7e6f829a51d430
Yudhishthir <ybhunwal@gmail.com>
2023-01-29 02:09:46 +0530

    [Script File]
    - Add a src to the script tag in the html file and used an
      external file for writing JavaScript.

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,21 @@
+let js = "amazing";
+console.log(40 + 79 - 32 * 2);
+
+// Referencing a variable.
+let firstName = "Yudhishthir";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let bed_breakfast = "Air BnB"; // Cannot use symbols in names.
+let $function = 34; // Cannot use reserved words as names, but if we still want to then we prefix the keyword by '_' or '$'.
+
+let person = "Jonas" // Should not use Person as it is a common notation for Classes (in OOP).
+let PI = 3.1415; // We use all caps for constants that are never going to change.
+
+// Use descriptive names rather than vague names.
+let myFirstJob = "Ruby on Rails Developer"; // Descriptive.
+let mySecondJob = "React Developer"; // Descriptive.
+
+let job1 = "Ruby on Rails Developer"; // Vague.
+let job2 = "React Developer"; // Vague.
