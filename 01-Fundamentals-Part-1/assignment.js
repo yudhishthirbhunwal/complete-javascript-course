0191f2eb39c493e4094357f5d374d5e16fccb9a3
Yudhishthir <ybhunwal@gmail.com>
2023-01-29 20:45:50 +0530

    [Assignments]

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -1,5 +1,32 @@
-let country = "India";
-let continent = "Asia";
-let population = 14076000000;
+// LECTURE: Values and Variables
+const country = "India";
+const continent = "Asia";
+let population = 1407600000;
 
 console.log(country, continent, population);
+
+// LECTURE: Data Types
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// LECTURE: let, const and var
+language = "Hindi";
+
+// LECTURE: Basic Operators
+console.log(population / 2);
+
+population++;
+console.log(population / 2);
+
+let populationFinland = 6000000;
+console.log(population > populationFinland);
+
+let averagePopulationOfACountry = 33000000;
+console.log(averagePopulationOfACountry > population);
+
+let description = 'India is in Asia, and its 703 million people speak Hindi';
